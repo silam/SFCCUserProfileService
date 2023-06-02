0a30a563e3eb6781746c98f6421fd35782b01da9
Si Lam <silam@hotmail.com>
2023-06-02 18:57:29 -0500

    Init

---
+++ SFCCUserProfileService/UserProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFCCUserProfileService
+{
+    public class UserProfile
+    {
+        public string id { get; set; }
+        public string first_name { get; set; }
+
+        public string last_name { get; set; }
+
+        public string email { get; set; }
+
+        public string billing_address { get; set; }
+
+        public string billing_zipcode { get; set; }
+
+        public string billing_state { get; set; }
+
+
+
+    }
+}
